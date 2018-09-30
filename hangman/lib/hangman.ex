1ff1834cb2c57b8f57c6478fef9c81215cc73782
Sabrina Peng <sabrina.h.peng@gmail.com>
2018-09-30 15:15:47 -0500

    added new_game and tally functionality to initialize game

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -12,7 +12,42 @@ defmodule Hangman do
       :world
 
   """
-  def hello do
-    :world
+  defstruct(
+    game_state: :initializing,
+    turns_left: 7,
+    used: [],
+    word: "",
+    word_letters: [],
+    letters: []
+  )
+
+  def new_game() do
+    new_word = Dictionary.random_word()
+    word_letters = string_to_list(new_word)
+    game = %Hangman{
+      word: new_word, 
+      word_letters: word_letters, 
+      letters: populate_letters(string_to_list(new_word), [])
+    }
+    tally(game)
   end
+
+  def string_to_list(word) do
+    word |> String.codepoints()
+  end
+
+  def populate_letters([], letters) do letters end
+  def populate_letters([_h | t], letters) do
+    populate_letters(t, ["_" | letters])
+  end
+
+  def tally(game) do
+    %{
+      game_state: game.game_state,
+      turns_left: game.turns_left,
+      letters: game.letters,
+      used: game.used
+    }
+  end
+
 end
