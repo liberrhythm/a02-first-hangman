f6f7932deac3f7a29d29affe645747e63406fc26
Sabrina Peng <sabrina.h.peng@gmail.com>
2018-09-30 16:52:23 -0500

    preliminary version of adding guess to used, reducing # of turns, and handling good/bad guesses

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -24,10 +24,10 @@ defmodule Hangman do
   def new_game() do
     new_word = Dictionary.random_word()
     word_letters = string_to_list(new_word)
-    game = %Hangman{
+    %Hangman{
       word: new_word, 
       word_letters: word_letters, 
-      letters: populate_letters(string_to_list(new_word), [])
+      letters: populate_letters(word_letters, [])
     }
     # tally(game)
   end
@@ -53,4 +53,69 @@ defmodule Hangman do
     }
   end
 
+  # see if letter has been guessed already
+  def handle_used(true, game, guess) do
+    IO.puts "Letter #{guess} already used!"
+    tally(game)
+  end
+
+  def handle_used(false, game, guess) do
+    game = %Hangman { game | 
+      turns_left: turns_left - 1,
+      game.used = [guess | game.used] |> Enum.sort()
+    })
+    handle_guess(Enum.member?(game.word_letters, guess), guess, game)
+  end
+
+  def guess_in_word(true, game) do
+    %Hangman { game | 
+      game_state: :good_guess,
+      letters: 
+    })
+  end
+  def guess_in_word(false, game) do
+    tally(%Hangman { game | 
+      game_state: :bad_guess
+    })
+  end
+
+  def letter_in_used(letter, used) do 
+    Enum.member?(used, letter)
+  end
+
+  def replace_letters(used, letters) do
+    Enum.map(letters, fn(x) -> 
+      if letter_in_used(x, used) do x
+      else "_"
+      end
+    end)
+  end 
+
+  def handle_good_guess(game) do
+    check_game_state(%Hangman { game | 
+      replace_letters(game, game.letters)
+    })
+  end
+
+  def handle_guess(false, guess, game) do
+    tally(%Hangman { game | 
+      game_state: :bad_guess
+    })
+  end
+  
+  def handle_guess(true, guess, game) do
+    tally(%Hangman { handle_good_guess(game) | 
+      game_state: :good_guess
+    })
+  end
+
+  # implement check_game_state and update_game_state
+
+  def make_move(game, guess) do
+    handle_used(Enum.member?(game.used, guess), game, guess)
+  end
+
 end
+
+
+
